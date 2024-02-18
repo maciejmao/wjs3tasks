db2a68387e97094c7aa54238bad3d1babadcc594
maciejmao <maciejmao@gmail.com>
2024-02-18 20:35:52 +0100

    menu - add css onhover / js events optional

--- menu/menu.js
+++ menu/menu.js
@@ -1,25 +1,17 @@
-const subEls = document.body.querySelectorAll('#menu > ul > li > ul');
-subEls.forEach((item) => {
-  item.className = 'submenu';
+const hover = true; // switch - css hover/js events on menu
+if (!hover) {
+  const menu = document.getElementById('menu');
+  menu.classList.remove('is-hover');
+  const subEls = document.querySelectorAll('#menu > ul > li > ul');
+  subEls.forEach((item) => {
+    if (!hover) {
+      item.parentNode.addEventListener('mouseenter', function () {
+        item.parentNode.classList.toggle('active');
+      });
 
-  const arrow = document.createElement('span');
-  arrow.className = 'arrow';
-  arrow.innerHTML = 'up or down arrow';
-
-  const marker = document.createElement('span');
-  marker.className = 'marker';
-  marker.innerHTML = 'parent marker';
-  marker.style.left = item.parentNode.offsetLeft + 'px';
-
-  item.parentNode.className = 'has-submenu';
-  item.parentNode.insertBefore(arrow, item);
-  item.parentNode.insertBefore(marker, item);
-
-  item.parentNode.addEventListener('mouseenter', function () {
-    item.parentNode.classList.toggle('active');
+      item.parentNode.addEventListener('mouseleave', function () {
+        item.parentNode.classList.toggle('active');
+      });
+    }
   });
-
-  item.parentNode.addEventListener('mouseleave', function () {
-    item.parentNode.classList.toggle('active');
-  });
-});
+}
